f199916fb4fc04190c99af97386c4268f1bb802a
YasminVenceslau <yasmin.venceslau@hotmail.com>
2024-06-20 00:39:04 -0300

    20/06

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('#carrosel').slick({
+        
+        dots: true,
+        speed: 50,
+        Infinite: false,
+        arrows: false,
+        
+    })
+
+    $('.giro').slick({
+        slideToShow: 1,
+        arrows: false,
+
+    })
+
+})

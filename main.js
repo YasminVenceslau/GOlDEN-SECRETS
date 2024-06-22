2d47580277935dfeea4d20404200791e39d75137
YasminVenceslau <yasmin.venceslau@hotmail.com>
2024-06-22 00:56:58 -0300

    22/06

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 $(document).ready(function(){
-    $('#carrosel').slick({
+    $('#carrosel').slick({ // faz o carrosel na sessão carrosel
         
-        dots: true,
-        speed: 50,
-        Infinite: false,
-        arrows: false,
+        dots: true, //adiciona pontos abaixo da imagem
+        speed: 50,  // indica a velocidade do giro 
+        Infinite: false, //  impede que o carrosel reinicie
+        arrows: false, // remove as setas
         
     })
 
-    $('.giro').slick({
-        slideToShow: 1,
-        arrows: false,
+    $('.giro').slick({ // faz o carrosel na sessão giro 
+        slideToShow: 1, //mostra uma imagem por vez 
+        arrows: false, // remove as setas
 
     })
+
+
+   function setupHandlers(buttonId, contentClass){
+    $(`#${buttonId}`).click(function(){
+        $(`.${contentClass}`).removeAttr('id').slideDown()
+    });
+
+    $(`.${contentClass}`).mouseleave(function(){
+        setTimeout(function(){
+            $(`.${contentClass}`).attr('id', 'esc')
+        }, 200)
+    })
+
+   }
+
+    setupHandlers('make', 'make');
+    setupHandlers('perf', 'perf');
+    setupHandlers('hair', 'hair');
+   setupHandlers('golden', 'golden');
+    
 
 })

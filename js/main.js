b6545f9e5f8fbcc57ba23dffe4341ca695cc42c7
YasminVenceslau <yasmin.venceslau@hotmail.com>
2024-07-02 20:29:37 -0300

    ok

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
     $('.giro').slick({ // faz o carrosel na sessão giro 
         slideToShow: 1, //mostra uma imagem por vez 
         arrows: false, // remove as setas
-        centerMode: true,
+        
         
 
     })
@@ -56,7 +56,7 @@ $(document).ready(function(){
 initializeSlick()
 
 
-    $('.buy').on('mouseenter' , function(){
+    $('.buy').on('mouseenter' , function(){ /*  mostra o botão*/
         $(this).siblings('div').find('button').removeAttr('id')
         $('.giro').mouseleave(function(){
             $('.butao').attr('id' , 'compra')
@@ -64,12 +64,12 @@ initializeSlick()
     })
 
 
-    function setupHandlers(buttonId, contentClass){
-    $(`#${buttonId}`).mouseenter(function(){
+    function setupHandlers(buttonId, contentClass){ /* mostra o seguinte o menu do cabeçalho */
+    $(`#${buttonId}`).click(function(){
         $(`.${contentClass}`).removeAttr('id').slideDown(100)
     });
 
-    $(`.${contentClass}`).mouseleave(function(){
+    $(`.${contentClass}`).dblclick(function(){
         setTimeout(function(){
             $(`.${contentClass}`).attr('id', 'esc')
         }, 200)
@@ -90,11 +90,11 @@ initializeSlick()
 
     
 
-    $('#tel').mask('(00) 00000-0000',{
+    $('#tel').mask('(00) 00000-0000',{ /* mascara do telefone */
         placeholder: '(00) 00000-0000'}
     )
     
-    $('form').validate({
+    $('form').validate({ /* validação do tex tel e email */
         rules: {
             email: {
                 required: true,
@@ -108,13 +108,13 @@ initializeSlick()
                 required: true
             },
         }, 
-        messages: {
+        messages: { /* mudanbça no texto da validação */
             text: 'Digite a ID do item',
             tel: 'Digite seu número do celular',
             email: 'Digite seu e-mail'
         },
 
-        submitHandler: function(form) {
+        submitHandler: function(form) { /* diz que a vcalidação está errdad */
             console.log(form)
         },
 
@@ -129,8 +129,8 @@ initializeSlick()
     })
 
 
-    $('.lista-itens a').click(function(){
-        const destino = $('#sobre');
+    $('.lista-itens a').click(function(){ /* ao clilcar em comprar ,vai direto para a parte da compra e add o id automaticamente */
+        const destino = $('#finalize');
 
         const nomeid = $(this).parent().find('h6 a').text();
         $('#text').val(nomeid)
@@ -141,7 +141,7 @@ initializeSlick()
         }, 1000)
     })
 
-    $('#Loja').click(function(){
+    $('#Loja').click(function(){ /* clique direto  no sobre a loja */
         const destino = $('#sobre');
 
         
